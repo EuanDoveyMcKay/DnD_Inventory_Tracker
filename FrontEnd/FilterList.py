06e1a25b1f4c1394c94e2ac2c36997e935d909b8
Euan Dovey-McKay <euan.dovey-mckay@hotmail.com>
2024-03-25 20:02:58 +0000

    Added tick boxes to 'FilterList.py'

--- FrontEnd/FilterList.py
+++ FrontEnd/FilterList.py
@@ -5,25 +5,51 @@ tk.set_appearance_mode("dark")
 tk.set_default_color_theme("dark-blue")
 
 root = tk.CTk()
-
-FilterFrame = tk.CTkFrame(  master=root,
-                            border_width=2,
-                            border_color="darkgrey")
+TitleFont = tk.CTkFont(family="Office", weight="bold", underline=True, size=30)
+TextFont = tk.CTkFont(family="Office", size=15)
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-TitleFont = tk.CTkFont(family="Office", weight="bold", underline=True, size=20)
+FilterFrame = tk.CTkFrame(  master=root,
+                            border_width=2,
+                            border_color="darkgrey",
+                            fg_color="gray10")
+
+CheckboxFrame = tk.CTkFrame(    master=FilterFrame,
+                                fg_color="gray10")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 FilterTitle = tk.CTkLabel(  master=FilterFrame,
-                            width= 20,
-                            height= 10,
-                            corner_radius=10,
                             text="Filter",
                             font=TitleFont,
                             justify="center")
 
+ItemFilter = tk.CTkCheckBox(    master=CheckboxFrame,
+                                text="Items",
+                                font=TextFont)
+
+WeaponFilter = tk.CTkCheckBox(  master=CheckboxFrame,
+                                text="Weapons",
+                                font=TextFont)
+
+ArmourFilter = tk.CTkCheckBox(  master=CheckboxFrame,
+                                text="Armour",
+                                font=TextFont)
+
+ContainerFilter = tk.CTkCheckBox(   master=CheckboxFrame,
+                                    text="Containers",
+                                    font=TextFont)
+
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
-FilterTitle.pack(padx=50,pady=10)
+FilterTitle.pack(padx=25,pady=(5,20))
+
+ItemFilter.grid(row=0, column=0, padx=(10,0), pady=(0,20))
+WeaponFilter.grid(row=0, column=1, padx=(0,10), pady=(0,20))
+ArmourFilter.grid(row=1, column=0, padx=(10,0), pady=(0,20))
+ContainerFilter.grid(row=1,column=1, padx=(0,10), pady=(0,20))
+CheckboxFrame.pack(padx=20,pady=(0,10))
+
 FilterFrame.pack()
 root.mainloop()
--- FrontEnd/FrontEnd.py
+++ FrontEnd/FrontEnd.py
@@ -6,3 +6,5 @@ tk.set_default_color_theme("dark-blue")
 root = tk.CTk()
 root.after(0, lambda: root.state('zoomed')) # Setting it to full screen (bordered)
 root.title("DnD Inventory Manager")
+
+root.mainloop()
